a5943d04b3c7e90d835c5a262572fb8de8ebf669
John Gramila <tynanjk@gmail.com>
2016-07-27 15:28:25 -0500

    divs wrapped

--- public/scripts/resume_generator.js
+++ public/scripts/resume_generator.js
@@ -1,33 +1,37 @@
-var ResumeHeader = React.createClass({
+var Resume = React.createClass({
   render: function() {
     return (
       <div className="resumeContainer">
         <h1>John Gramila's Resume</h1>
-          <ResumerWapper/>
+        <ResumeWapper/>
       </div>
     );
   }
 });
 
 var ResumeWapper = React.createClass({
+  toggle: function(){
+    this.setState({isHidden: !this.state.isHidden});
+  },
   render: function() {
     return (
       <div className="resumeWrapper">
-        {!this.props.isHidden? <ResumeHeader title="Languages"/> : <LanguageResumeItem/>}
-        {!this.props.isHidden? <ResumeHeader title="Experience"/> : <ExperienceResumeItem/>}
-        {!this.props.isHidden? <ResumeHeader title="Education"/> : <EducationResumeItem/>}
+        {!this.props.isHidden? <ResumeHeader title="Languages" onClick={this.toggle}/> : <LanguageResumeItem onClick={this.toggle}/>}
+        {!this.props.isHidden? <ResumeHeader title="Experience" onClick={this.toggle}/> : <ExperienceResumeItem onClick={this.toggle}/>}
+        {!this.props.isHidden? <ResumeHeader title="Education" onClick={this.toggle}/> : <EducationResumeItem onClick={this.toggle}/>}
       </div>
     );
   }
 });
 
 var ResumeHeader = React.createClass({
+  getInitialState: function(){
+    return {isHidden: false};
+  },
   render: function() {
     return (
       <div className="resumeHeader">
-        {this.props.title }
-        {!this.props.isHidden? <ResumeHeader/> : <LanguageResumeItem/>}
-        {!this.props.isHidden? <ResumeHeader/> : <LanguageResumeItem/>}
+        <h2>{this.props.title}</h2>
       </div>
     );
   }
@@ -38,18 +42,13 @@ var ResumeHeader = React.createClass({
 var LanguageResumeItem = React.createClass({
   render: function() {
     return (
-      <div className="resumeWrapper">
-        <div className="visibleResumeContainer">
-          <h1>Languages</h1>
-        </div>
-        <div className="hiddenResumeContainer">
-          <h1>Languages</h1>
-            <ul>
-              <li>Proficient: Ruby, Rails, HTML, CSS, Command Line</li>
-              <li>Experienced: Javascript, JQuery, Git, Bootstrap, React</li>
-              <li>Familiar: GIMP,  SQL, Excel</li>
-            </ul>
-        </div>
+      <div className="resumeContent">
+        <h1>Languages</h1>
+          <ul>
+            <li>Proficient: Ruby, Rails, HTML, CSS, Command Line</li>
+            <li>Experienced: Javascript, JQuery, Git, Bootstrap, React</li>
+            <li>Familiar: GIMP,  SQL, Excel</li>
+          </ul>
       </div>
     );
   }
@@ -58,20 +57,15 @@ var LanguageResumeItem = React.createClass({
 var ExperienceResumeItem = React.createClass({
   render: function() {
     return (
-      <div className="resumeWrapper">
-        <div className="visibleResumeContainer">
-          <h1>Experience</h1>
-        </div>
-        <div className="hiddenResumeContainer">
-          <h2>Experience</h2>
-            <ul>
-              <h3>SpeakEasy - Development Intern</h3>
-              <h3>Startup Institute - Web Development Bootcamp</h3>
-              <h3>Groupon - Web Content Specialist</h3>
-              <h3>Groupon - Deal Qualifier</h3>
-              <h3>Windy City Weasels - Chief of Public Information</h3>
-            </ul>
-        </div>
+      <div className="resumeContent">
+        <h2>Experience</h2>
+          <ul>
+            <h3>SpeakEasy - Development Intern</h3>
+            <h3>Startup Institute - Web Development Bootcamp</h3>
+            <h3>Groupon - Web Content Specialist</h3>
+            <h3>Groupon - Deal Qualifier</h3>
+            <h3>Windy City Weasels - Chief of Public Information</h3>
+          </ul>
       </div>
     );
   }
@@ -80,17 +74,12 @@ var ExperienceResumeItem = React.createClass({
 var EducationResumeItem = React.createClass({
   render: function() {
     return (
-      <div className="resumeWrapper">
-        <div className="visibleResumeContainer">
-          <h1>Education</h1>
-        </div>
-        <div className="hiddenResumeContainer">
-          <h2>Education</h2>
-          <ul>
-            <li>Lawrence University, Appleton- Philosophy, English: 2004 – 2008</li>
-            <li>Startup Institute - Web Development Bootcamp: 2016</li>
-          </ul>
-        </div>
+      <div className="resumeContent">
+        <h2>Education</h2>
+        <ul>
+          <li>Lawrence University, Appleton- Philosophy, English: 2004 – 2008</li>
+          <li>Startup Institute - Web Development Bootcamp: 2016</li>
+        </ul>
       </div>
     );
   }
@@ -98,6 +87,6 @@ var EducationResumeItem = React.createClass({
 
 
 ReactDOM.render(
-  <ResumeHeader/>,
+  <Resume/>,
   document.getElementById('content')
 );
